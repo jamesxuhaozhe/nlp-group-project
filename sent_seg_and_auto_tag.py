bbd0bb4cc9c510ae0cb61965cce6f0b937f76f42
haozhexu <haozhexu@ubuntu.com>
2015-11-18 22:05:20 -0800

    Still clearning the fu*king data hengheng

---
+++ sent_seg_and_auto_tag.py
@@ -0,0 +1,139 @@
+from nltk.tokenize import sent_tokenize
+from nltk import wordpunct_tokenize
+import sys
+
+"""
+1. Detect entences within each line.
+2. For each sentence, label it with its preposition usage and also the position of\
+preposition.
+3. write each sentence example to line.
+4. the output file will be like:
+like 2 He is a good boy, like he should be.
+.......
+"""
+
+def check_arg():
+    """
+    Check the validity of the system arguments.
+    """
+
+    if len(sys.argv) != 2:
+        print 'The script you are running should take in 2 arguments.\
+        Should run like this: python script.py whater.data'
+        sys.exit()
+
+def begin_work():
+    """
+    Top level caller function.
+    """
+    pass
+
+def extract_examps_from_file(file_path):
+    """
+    Extract all the example from the given file.
+
+    @param: file_path path the given file
+    @type: file_path str
+
+    @return: list of list, each element list contain example sents for a line
+    @type: list
+    """
+
+    examps = []
+    try:
+        with open(file_path, 'r') as f:
+            for line in f:
+                examps.append(extract_examps_from_line(line))
+    except IOError:
+        print 'Something is wrong.'
+        pass
+
+    return examps
+
+def extract_examps_from_line(line):
+    """
+    Extract all the example sents from the given line.
+
+    @param: line each line from the file containing multiple example sents.
+    @type: line str
+
+    @return: a list containing all the example sents from the line
+    @type: list
+    """
+
+    examps = []
+    pass
+
+def extract_sents_from_line(line):
+    """
+    Extract sentences from the given line.
+
+    @param: line line that may contain multiple sentences.
+    @type: str
+
+    @return: list of sentences
+    @type: list
+    """
+
+    sents = []
+
+    try:
+        sents = sent_tokenize(line)
+    except:
+        pass
+
+    return sents
+
+def tokenize_sent(sent):
+    """
+    Tokenize a sentence, including the punctuations.
+
+    @param: sent sentence to be tokenized
+    @type: sent str
+
+    @return: a list of tokens for the given sentences
+    @type: list
+    """
+
+    tokens = []
+
+    try:
+        tokens = wordpunct_tokenize(sent)
+    except:
+        pass
+
+    return tokens
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
